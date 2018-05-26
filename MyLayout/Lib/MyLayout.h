3519b04a246650d94fabfec2e07c06153b677386
欧柏泉 <oubaiquan@meituan.com>
2018-05-26 11:55:15 +0800

    support for carthage

--- MyLayout/Lib/MyLayout.h
+++ MyLayout/Lib/MyLayout.h
@@ -37,7 +37,7 @@
  */
 
 
-//Current version is 1.5.3, please open: https://github.com/youngsoft/MyLinearLayout/blob/master/CHANGELOG.md to show the changes.
+//Current version is 1.5.4, please open: https://github.com/youngsoft/MyLinearLayout/blob/master/CHANGELOG.md to show the changes.
 
 
 #ifndef MyLayout_MyLayout_h
